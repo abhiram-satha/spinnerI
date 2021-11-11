05b2f45f61f3c8241e7ab8bf8360d0ca28caa7c4
Abhiram <abhiram.satha@gmail.com>
2021-11-11 00:59:09 +0000

    Revised spinner2 and spinner1

--- spinner2.js
+++ spinner2.js
@@ -1,12 +1,9 @@
 const spinnerArray = ['|     ', '/     ', '-     ','\\    '];
-
 let timer = 100;
-
 for (const symbol of spinnerArray) {
   setTimeout(() => {
     process.stdout.write(`\r ${symbol}`);
 }, timer)
-
   timer +=300
 }
 
